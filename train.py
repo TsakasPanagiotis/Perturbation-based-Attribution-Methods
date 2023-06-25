cced3ff6aee0c39b89a00111541d1706bbb7ec1c
Nick Apostolikas <n.apostolikas@hotmail.com>
2023-06-25 12:58:19 +0200

    structure change

--- train.py
+++ train.py
@@ -1,27 +1,6 @@
 from tqdm import tqdm
 import torch
 
-def create_subset(args, dataset):
-    subset = []
-    if args.experiment == 'pneumonia':
-        num_images_per_class = 25
-        class_counts = [0] * 4
-    elif args.experiment == 'tumor':
-        num_images_per_class = 50
-        class_counts = [0] * 2
-    elif args.experiment == 'stl':
-        num_images_per_class = 10
-        class_counts = [0] * 10
-
-    for image, label in dataset:
-        if class_counts[label] < num_images_per_class:
-            subset.append((image, label))
-            class_counts[label] += 1
-        if all(count >= num_images_per_class for count in class_counts):
-            break
-    return subset
-
-
 def evaluate_model(args,model,dataloader):
     correct = 0
     total = 0
